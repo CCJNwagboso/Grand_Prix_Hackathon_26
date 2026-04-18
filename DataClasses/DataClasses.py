4614657bdb76392c1b3918ea9b0792f34b7ff889
leroDipper <marewangepoleroy2001@gmail.com>
2026-04-18 14:31:47 +0200

    data classes done

---
+++ DataClasses/DataClasses.py
@@ -0,0 +1,41 @@
+from dataclasses import dataclass
+
+@dataclass
+class car :
+    max_speed: float
+    accel: float
+    brake: float
+    limp_constant: float
+    crawl_constant: float
+    fuel_tank_capacity: float
+    initial_fuel: float
+    fuel_consumption: float
+
+
+@dataclass
+class track :
+    id : int
+    type : str
+    length_m: float
+
+
+@dataclass
+class tyres:
+    type: str
+    life_span: float
+    dry_friction_multiplier: float
+    cold_friction_multiplier: float
+    light_rain_friction_multiplier: float
+    heavy_rain_friction_multiplier: float
+    dry_degradation: float
+    cold_degradation: float
+    light_rain_degradation: float
+    heavy_rain_degradation: float
+
+
+@dataclass
+class weather:
+    condition: str
+    duration_s: float
+    acceleration_multiplier: float
+    deceleration_multiplier: float
---
+++ main.py
@@ -0,0 +1,6 @@
+from DataClasses.DataClasses import car, track, tyres, weather
+import numpy as np
+
+
+
+
