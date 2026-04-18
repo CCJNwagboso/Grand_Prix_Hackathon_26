d694fadc3e00be81da0ca79fcd5bbeb744b4239f
leroDipper <marewangepoleroy2001@gmail.com>
2026-04-18 14:50:38 +0200

    I do actions, Chika does constraints

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-from DataClasses.DataClasses import car, track, tyres, weather
+from DataClasses.dataClasses import car, track, tyres, weather
 import numpy as np
+from Methods.Equations import MaxCornerSpeed, TimeToAccelarate, TotalStraightDegradation, TotalBreakingDegradation, TotalCornerDegradation, TyreFriction, FuelUsed, RefuelTime, BrakingDistance, BrakingTime, PitStopTime, baseScore, FuelBonus, TyreBonus
+
+
 
 
 
